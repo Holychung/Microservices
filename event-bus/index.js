84709951d0aafdf42a4ee60479e48cb2fd41d4a1
Holychung <mickey94378@gmail.com>
2022-05-07 23:01:22 +0800

    Add load balancer services

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch(err => console.log(err)); // posts
-  axios.post('http://localhost:4001/events', event).catch(err => console.log(err));; // comments
-  axios.post('http://localhost:4002/events', event).catch(err => console.log(err)); // query
-  axios.post('http://localhost:4003/events', event).catch(err => console.log(err));; // moderation
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => console.log(err)); // posts
+  axios.post('http://comments-srv:4001/events', event).catch(err => console.log(err));; // comments
+  axios.post('http://query-srv:4002/events', event).catch(err => console.log(err)); // query
+  axios.post('http://moderation-srv:4003/events', event).catch(err => console.log(err));; // moderation
 
   res.send({ status: 'OK' });
 });
